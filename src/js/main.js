d0de22be86f6527acd0ab1ab86f78712d0bb373f
Nikolay Tarasenko <kolyatarasenko@ukr.net>
2019-09-10 14:20:40 +0300

    Footer, hamburger, mobile version and some structure fixes was added

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+const menuButton = document.querySelector('.hamburger');
+const menu = document.querySelector('.header__nav');
+
+function toggleMenu(e) {
+    e.preventDefault();
+
+    this.classList.toggle('active');
+
+    if (this.classList.contains('active')) {
+        menu.classList.add('active');
+
+        document.body.style.overflow = 'hidden';
+    } else {
+        menu.classList.remove('active');
+        document.body.style.overflow = 'auto';
+    }
+};
+
+function showMenu() {
+    if (document.documentElement.clientWidth <= 751 || window.innerWidth <= 768) {
+        menu.classList.add('mainMenu');
+    } else {
+        menu.classList.remove('mainMenu');
+    }
+}
+
+showMenu();
+
+window.addEventListener('resize', showMenu);
+menuButton.addEventListener('click', toggleMenu);
